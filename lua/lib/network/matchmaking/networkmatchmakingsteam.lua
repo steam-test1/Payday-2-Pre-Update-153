73db81431fc903947187eec25716957f699d39e6
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2024-09-12 08:16:12 +0100

    108.1

--- lua/lib/network/matchmaking/networkmatchmakingsteam.lua
+++ lua/lib/network/matchmaking/networkmatchmakingsteam.lua
@@ -1,6 +1,6 @@
 NetworkMatchMakingSTEAM = NetworkMatchMakingSTEAM or class()
 NetworkMatchMakingSTEAM.OPEN_SLOTS = 4
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "payday2_v1.54.13"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "payday2_v1.54.14"
 
 function NetworkMatchMakingSTEAM:init()
 	cat_print("lobby", "matchmake = NetworkMatchMakingSTEAM")
